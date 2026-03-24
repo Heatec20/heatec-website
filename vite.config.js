dda6381c57e106d3ed210a5cf99d7cda54da465d
Heatec20 <heatec20@gmail.com>
2026-03-24 13:42:55 +0200

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()]
+  
+½!anorigin: 'http://localhost:5173'
+  //}\)
++
+import { config } from 'dotenv';
+
+  // Environment variables
+  const BACEND_URL = process.env.VITE_BACEND_URL || ''jnƒ$mcsvaBUYSR24Ns=http://localhost:3000';
+});"}
