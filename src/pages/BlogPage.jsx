bc858dee49670f28b3f2f035c3fa4a823f18582d
Heatec20 <heatec20@gmail.com>
2026-03-24 13:42:15 +0200

    Add src/pages/BlogPage.jsx

---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,9 @@
+import { useTranslation } from 'react-i18next';
+
+export default function BlogPage() {
+  const { t } = useTranslation();
+  return (
+    <div className="space-y-12">
+    </div>
+  );
+};
