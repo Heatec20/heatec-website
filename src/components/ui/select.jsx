bd19f02d6340e101c249f02f1aa622f524888cf5
Heatec20 <heatec20@gmail.com>
2026-03-24 13:53:45 +0200

    Add src/components/ui/select.jsx

---
+++ src/components/ui/select.jsx
@@ -0,0 +1,8 @@
+import * as SelectPrimitive from "@radix-ui/react-select"
+import { ChevronDown } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+const Select = SelectPrimitive.Root
+
+export { Select, SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectItem, SelectLabel, SelectScrollUpButton, SelectScrollDownButton }
