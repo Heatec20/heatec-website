fdf91e90e8882afb5a784c5da1cef0f58485ba17
Heatec20 <heatec20@gmail.com>
2026-03-24 13:54:29 +0200

    Add src/pages/SpaceHeatingPage.jsx

---
+++ src/pages/SpaceHeatingPage.jsx
@@ -0,0 +1,15 @@
+import { Helmet } from 'react-helmet-async'
+import SolutionsHero from '@/components/solutions/SolutionsHero'
+import ProductSection from '@/components/solutions/ProductSection'
+import ProjectCTA from '@/components/projects/ProjectCTA'
+
+export default function SpaceHeatingPage() {
+  return (
+    <>
+      <Helmet>
+        <title>Space Heating Solutions - Heatec</title>
+      </Helmet>
+      <SolutionsHero solution="space" />
+      <ProductSection solution="space" />
+      <ProjectCTA />
+          
