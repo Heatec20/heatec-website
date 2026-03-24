f38d187895a24d264c17637b722ac4b22fded199
Heatec20 <heatec20@gmail.com>
2026-03-24 13:42:18 +0200

    Add src/pages/FireplacesPage.jsx

---
+++ src/pages/FireplacesPage.jsx
@@ -0,0 +1,9 @@
+import { useTranslation } from 'react-i18next';
+
+export default function FireplacesPage() {
+  const { t } = useTranslation();
+  return (
+    <div className="space-y-12">
+    </div>
+  );
+};
