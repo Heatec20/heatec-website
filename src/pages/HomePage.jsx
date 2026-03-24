abbf6febbc363213599794e1895ee333338b37e3
Heatec20 <heatec20@gmail.com>
2026-03-24 13:42:19 +0200

    Add src/pages/HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import { useTranslation } from 'react-i18next';
+
+export default function HomePage() {
+  const { t } = useTranslation();
+  return (
+    <div className="space-y-12">
+    </div>
+  );
+};
