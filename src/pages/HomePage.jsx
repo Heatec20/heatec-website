acef7ad43ef568af817bb846de4a3e2f5b3dc102
Heatec20 <heatec20@gmail.com>
2026-03-25 05:19:43 +0200

    fix: restore full HomePage with all sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,25 @@
-import { useTranslation } from 'react-i18next';
+import Hero from '@/components/home/Hero.jsx'
+import StatsBar from '@/components/home/StatsBar.jsx'
+import WhoWeAre from '@/components/home/WhoWeAre.jsx'
+import TrustedBrands from '@/components/home/TrustedBrands.jsx'
+import SolutionsOverview from '@/components/home/SolutionsOverview.jsx'
+import Benefits from '@/components/home/Benefits.jsx'
+import FireplacesFeature from '@/components/home/FireplacesFeature.jsx'
+import FeaturedProjects from '@/components/home/FeaturedProjects.jsx'
+import GetInTouch from '@/components/home/GetInTouch.jsx'
 
 export default function HomePage() {
-  const { t } = useTranslation();
   return (
-    <div className="space-y-12">
+    <div className="flex flex-col w-full">
+      <Hero />
+      <StatsBar />
+      <WhoWeAre />
+      <TrustedBrands />
+      <SolutionsOverview />
+      <Benefits />
+      <FireplacesFeature />
+      <FeaturedProjects />
+      <GetInTouch />
     </div>
-  );
-};
+  )
+}
